fc191c1c5bf06e549da0e0fa35427057d3369a1d
Attri99 <ksh09201@naver.com>
2024-11-29 13:02:04 +0900

    3교시

--- app03/src/main/java/com/min/app03/controller/MvcController1.java
+++ app03/src/main/java/com/min/app03/controller/MvcController1.java
@@ -3,6 +3,7 @@ package com.min.app03.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller // 요청과 응답을 처리하는 컨트롤러
 public class MvcController1 {
@@ -11,8 +12,8 @@ public class MvcController1 {
   /*
    * 반환타입
    *   1) String        : 사용자에게 보여 줄 (응답할) JSP 의 경로와 이름을 작성한다.
-   *   2) void
-   *   3) ModelAndView
+   *   2) void          : 요청한 주소를 JSP 경로와 이름으로 인식한다.
+   *   3) ModelAndView  : 
    *   4) ...
    *   
    * 메소드명
@@ -38,15 +39,41 @@ public class MvcController1 {
     *   
     *   
     * 3. 요청 메소드
-    *   1) method=RequestMethod.GET : GET 방식을 의미한다.
+    *   1) method=RequestMethod.GET : GET 방식을 의미한다. (디폴트)
     *   2) method=RequestMethod.POST : POST 방식을 의미한다.
     *  
     */
 
   @RequestMapping(value="/", method=RequestMethod.GET)
   public String welcome() {
-    return "index";
+    return "webdir1/index"; // return "/webdir1/index/"로 작성해도 스프링이 올바르게 해석해 준다.
+    /*
+     * return "webdir1/index"; 해석해보기
+     * 
+     * 1. 리턴 값 "index" 는 ViewResolver 에게 전달됩니다.
+     *    (DispatcherServlet : servlet-context.xml 에 ViewResolver가 정의되어 있다.)
+     * 2. ViewResolver는 "index" 앞에 "/WEB-INF/views/" 문자열을 추가합니다. (prefix)
+     * 3. ViewResolver는 "index" 뒤에 ".jsp" 문자열을 추가합니다. (suffix)
+     * 4. ViewResolver는 완성된 최종 view로 이동합니다.
+     *    (최종 View의 모습 : "/WEB-INF/views/webdir1/index")
+     * 
+     */
   }
-
-
+  
+  
+  @RequestMapping(value="/webdir1/list", method = RequestMethod.GET)
+  public void letsgo() {
+    // 반환타입이 void 이므로 요청 주소 "/webdir1/list"를 사용자에게 보여줄 JSP 경로와 이름으로 해석한다.
+    // ViewResolver 에 의해서 최종 경로는 "/WEB-INF/views/webdir1/list.jsp"가 됩니다.
+  }
+  
+  
+  @RequestMapping(value="/board/view") // method를 생략하면 GET 방식으로 동작한다.
+  public ModelAndView gogogo() {
+    
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("webdir1/detail"); // 사용자에게 보여줄 JSP의 경로와 이름. ViewResolver에 의해 처리된다.
+    mav.addObject("number", 10);  // JSP로  number = 10이라는 값을 전달합니다.
+    return mav;
+  }
 }
