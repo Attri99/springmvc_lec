885a708794ead142182974fbacd65beba49f30a7
Attri99 <ksh09201@naver.com>
2024-11-29 16:58:13 +0900

    7교시

--- app03/src/main/java/com/min/app03/controller/MvcController3.java
+++ app03/src/main/java/com/min/app03/controller/MvcController3.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.min.app03.vo.PageVo;
 
 @Controller
 public class MvcController3 {
@@ -81,6 +84,31 @@ public class MvcController3 {
    * 각 Parameter마다 @RequestParam Annotation을 추가하여 Parameter를 직접 변수로 받을 수 있음.
    * Parameter의 필수 여부와 디폴트 값 설정을 할 수 있다.
    * @RequestParam Annotation은 생략도 할 수 있다. 생략하면 선언된 변수로 추론하여 값을 받는다.
-   * 
    */
+  
+  // Query String : sort=ASC
+  @RequestMapping(value="/webdir3/req5")
+  public String req5(
+      @RequestParam(value="sort") String sort,  //parameter sort가 전달되지 않으면 잘못된 요청(400) 예외가 발생한다.
+      @RequestParam(value="page", required=false, defaultValue="1") int page // Parameter page가 전달되지 않으면 기본값으로 "1"을 사용합니다.
+      ) {
+        System.out.println(sort + ", " + page);
+        return"webdir3/req5";
+  }
+  
+  /*
+   * Query String 처리하기 3
+   * 커맨드 객체 활용하기
+   * Parameter를 필드로 가지고 있는 클래스 타입의 객체 (커맨드 객체)를 이용해서 Parameter를 받을 수 있다.
+   * 커맨드 객체에는 Setter가 정의되어 있어야 한다.
+   */
+  
+  // Query String : sort=ASC&page=1
+  @RequestMapping(value="/webdir3/req6")
+  public String req6(PageVo pageVo) {
+    System.out.println(pageVo.getSort() + ", " + pageVo.getPage());
+    return "webdir3/req6";
+  }
+  
+  
 }
--- app03/src/main/webapp/WEB-INF/views/webdir3/main.jsp
+++ app03/src/main/webapp/WEB-INF/views/webdir3/main.jsp
@@ -28,7 +28,8 @@
       <c:set var="contextPath" value="${pageContext.request.contextPath}"/>
       ${contextPath}
    --%>
-  <!-- 요청 만들기 1 : <a> 태그 -->
+   
+  <%-- 요청 만들기 1 : <a> 태그 (<a> 태그는 항상 GET 방식으로 요청한다. --%>
   <div>
     <a href="<%=request.getContextPath()%>/webdir3/req1">요청1</a>
     <br/>
@@ -37,6 +38,10 @@
     <a href="<%=request.getContextPath()%>/webdir3/req3?flowers=ROSE&flowers=TULIP">요청3</a>
     <br/>
     <a href="<%=request.getContextPath()%>/webdir3/req4">요청4</a>
+    <br/>
+    <a href = "<%=request.getContextPath()%>/webdir3/req5?sort=ASC">요청5</a>
+    <br/>
+    <a href ="<%=request.getContextPath()%>/webdir3/req6?sort=ASC&page=1">요청6</a>
   </div>
   
 </body>
