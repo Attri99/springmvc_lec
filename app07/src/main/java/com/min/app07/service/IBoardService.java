116ce1d41be42a32ab5494ea4930bd04e62167ab
Attri99 <ksh09201@naver.com>
2024-12-11 11:21:31 +0900

    2교시

---
+++ app07/src/main/java/com/min/app07/service/IBoardService.java
@@ -0,0 +1,13 @@
+package com.min.app07.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface IBoardService {
+
+  Map<String, Object> getBoardList(HttpServletRequest request);
+
+  
+  
+}
